9ff19c9d8849ed63a019b0f239e9a15b24e06b3e
AndreeaMihaelaP <andreeapatrascu1997@gmail.com>
2024-11-16 12:19:08 +0000

    feat(initialize): colors, fonts and icons

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
 import { Text, View } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import colors from "../theme/colors";
+import font from "../theme/fonts";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ color: colors.primary, fontSize: font.size.lg }}>
+        Hey world!
+      </Text>
+      {/* <Entypo name="cog" size={32} /> */}
     </View>
   );
 }
---
+++ theme/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  primary: "#405de6",
+
+  white: "#ffff",
+  black: "#0000",
+  grey: "#6666",
+  light: "#cccc",
+};
+
+export default colors;
